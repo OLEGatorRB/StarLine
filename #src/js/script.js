6bab85afabd15f36569610f81702284f5090d460
OLEGatorRB <olegatorrb@yandex.ru>
2021-11-24 17:28:43 +0300

    добавил Popup

---
+++ #src/js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".button");
+var popup = document.querySelector(".popup")
+
+button.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("not-show")
+	console.log("hello");
+})
