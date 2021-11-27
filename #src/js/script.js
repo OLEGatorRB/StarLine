2df4d60bf0309301280993b335b2d6d10a009af5
OLEGatorRB <olegatorrb@yandex.ru>
2021-11-27 22:29:04 +0300

    немного увеличил отступ от нижнего фото в контенете

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,7 @@
 var button = document.querySelector(".button");
 var popup = document.querySelector(".popup");
 
+/*закрываем popup*/
 button.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("not-show")
